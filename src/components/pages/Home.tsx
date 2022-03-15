7bb0cc47719cabfe6fb047e87c0f2d9f9f839bfe
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-03-15 20:03:48 +0530

    Add files via upload

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,1 +1,12 @@
-
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+export default function Home() {
+  return (
+    <div>
+        <div className='flex w-full'>
+            <div className='w-1/2'>Left</div>
+            <div className='w-1/2'>Right</div>
+        </div>
+    </div>
+  )
+}
