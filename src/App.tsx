066bd0b5bb4aa6adc5e3e382bf36f8fa157ccf4a
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-03-15 19:58:52 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
+export default App;
